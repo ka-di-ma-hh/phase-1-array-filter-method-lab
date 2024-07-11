4540a51a2b1820c657a027b89375ea37d65c6a69
junne.wanja@student.moringaschool.com <junne.wanja@student.moringaschool.com>
2024-07-11 15:30:05 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solution here
+
+{
+    const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+    const findMatching = (drivers, name) => {
+      return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+    }
+   
+    findMatching(drivers, 'Bobby')
+  }
+  {
+    const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby"];
+    const fuzzyMatch = (drivers, nameLetters) => {
+      return drivers.filter(driver => driver.toLowerCase().startsWith(nameLetters.toLowerCase()))
+    }
+    fuzzyMatch(drivers, 'sa')
+  }
+  
+  {
+    const drivers = [
+      {
+        name: "Bobby",
+        hometown: "Pittsburgh",
+      },
+      {
+        name: "Sammy",
+        hometown: "New York",
+      },
+      {
+        name: "Sally",
+        hometown: "Cleveland",
+      },
+      {
+        name: "Annette",
+        hometown: "Los Angeles",
+      },
+      {
+        name: "Bobby",
+        hometown: "Tampa Bay",
+      },
+    ];
+  
+    const matchName = (drivers, name) => {
+      return drivers.filter(driver => driver.name === name)
+    }
+    matchName(drivers,'Bobby')
+  }
